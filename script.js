2783fb317fa3fff61fd32b3063c4ee7e91e9c371
Henry Omofonmwan <omofonmwanhenrynosa@gmail.com>
2024-02-22 03:46:24 -0800

    FINISHED THE 3RD SPIRNT

--- script.js
+++ script.js
@@ -93,6 +93,63 @@ isLeapYear(2024)
 
 
 
+//##########################################################################
+
+//Task Number 5
+
+// Write a function that takes in a string of one or more words, and returns the same string, 
+// but with all words that have five or more letters reversed (Just like the name of this Kata). 
+// Strings passed in will consist of only letters and spaces. 
+// Spaces will be included only when more than one word is present.
+
+// Examples:
+
+// "Hey fellow warriors"  --> "Hey wollef sroirraw" 
+// "This is a test        --> "This is a test" 
+// "This is another test" --> "This is rehtona test"
+
+
+function spinWords(string){
+ let splitedString = string.split(" ")
+
+const modifiedArray = splitedString.map((x) => {
+    if (x.length > 4) {
+      
+        return x.split("").reverse().join("")
+    }
+  
+    return x
+ })
+
+ return modifiedArray.join(" ")
+}
+
+spinWords("Henry is a God in human lekelejsjs ejdjdjdidjddodod")
+
+
+
+///TEST
+
+// describe("Spinning words",()=>{
+//     it("Sample tests",()=>{
+//       assert.strictEqual(spinWords("Welcome"), "emocleW");
+//       assert.strictEqual(spinWords("Hey fellow warriors"), "Hey wollef sroirraw");
+//       assert.strictEqual(spinWords("This is a test"), "This is a test");
+//       assert.strictEqual(spinWords("This is another test"), "This is rehtona test");
+//       assert.strictEqual(spinWords("You are almost to the last test"), "You are tsomla to the last test");
+//       assert.strictEqual(spinWords("Just kidding there is still one more"), "Just gniddik ereht is llits one more");
+//       assert.strictEqual(spinWords("Seriously this is the last one"), "ylsuoireS this is the last one");
+
+
+
+
+
+
+
+
+
+
+
 
 
 
