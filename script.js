fb5ac1b4d78b0771c5f27bae4b48af3ef1353283
Henry Omofonmwan <omofonmwanhenrynosa@gmail.com>
2024-02-27 09:02:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ isLeapYear(2024)
 
 //##########################################################################
 
-//Task Number 5
+//Task Number 3
 
 // Write a function that takes in a string of one or more words, and returns the same string, 
 // but with all words that have five or more letters reversed (Just like the name of this Kata). 
@@ -139,6 +139,72 @@ spinWords("Henry is a God in human lekelejsjs ejdjdjdidjddodod")
 //       assert.strictEqual(spinWords("You are almost to the last test"), "You are tsomla to the last test");
 //       assert.strictEqual(spinWords("Just kidding there is still one more"), "Just gniddik ereht is llits one more");
 //       assert.strictEqual(spinWords("Seriously this is the last one"), "ylsuoireS this is the last one");
+
+
+///########################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///#############################################################################################
+
+// TASK Number 3
+
+// An Arithmetic Progression is defined as one in which there is a constant difference between 
+// the consecutive terms of a given series of numbers. 
+// You are provided with consecutive elements of an Arithmetic Progression. 
+// There is however one hitch: 
+// exactly one term from the original series is missing from the set of numbers which have been given to you. 
+// The rest of the given series is the same as the original AP. 
+// Find the missing term.
+
+// You have to write a function that receives a list, 
+// list size will always be at least 3 numbers. 
+// The missing term will never be the first or last one.
+
+// Example
+// findMissing([1, 3, 5, 9, 11]) == 7
+// PS: This is a sample question of the facebook engineer challenge on interviewstreet. 
+// I found it quite fun to solve on paper using math, derive the algo that way. Have fun!
+
+var findMissing = function (list) {  
+    console.log(list)
+    return list [0] ;
+    
+  }
+
+  findMissing([1,3,5,9]);
 
 
 
